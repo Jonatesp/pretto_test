b3a805597967cb20141d30e9b3c9dfa6db8da1d8
anbourge <anbourge@student.42.fr>
2024-07-26 00:28:55 +0200

    all questions done, project finished

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include <stdlib.h>
 #include <iostream>
-#include "second_question.cpp"
 #include "third_question.cpp"
 
 int main() {
-	double amount = 160000.0;
-	double rate = 0.018;
-	double duration = 300.0;
-	double monthly = monthly_calcul(amount,rate, duration);
+	double amount1 = 140000.0;
+	double rate1 = 0.038;
+	double duration1 = 264.0;
+	double amount2 = 160000.0;
+	double rate2 = 0.044;
+	double duration2 = 300.0;
+	double monthly = monthly_calcul(amount2,rate2, duration2);
 
-	std::cout << "monthly payment : " << monthly << std::endl;
-	std::cout << "total interests : " << interests_calcul(amount, rate, duration) << std::endl;
-	std::cout << "ratio : " << ratio(0.0115, 180.0, rate, duration) << std::endl;
+	std::cout << "Monthly payment : " << monthly << std::endl;
+	std::cout << "Total interests : " << interests_calcul(amount2, rate2, duration2) << std::endl;
+	std::cout << "Ratio : " << ratio(rate1, duration1, rate2, duration2) << std::endl;
+
+	//rate grid in a map with duration of the loan as key and rate as value
+	std::map<int, double> rate_grid;
+	rate_grid[10] = 2.9;
+	rate_grid[12] = 3.2;
+	rate_grid[15] = 3.5;
+	rate_grid[20] = 3.8;
+	rate_grid[22] = 3.8;
+	rate_grid[25] = 4.4;
+
+	best_interests(rate_grid, 25);
 }
--- second_question.cpp
+++ second_question.cpp
@@ -1,9 +1,15 @@
 #include "first_question.cpp"
 
 double ratio(double rate1, double duration1, double rate2, double duration2) {
+	//pourcentage du petit pret avec interets (m1) par rapport au capital du petit pret (m3)
 	double m1_percentage_of_m3 = (100.0 + interests_calcul(100.0, rate1, duration1)) / 100.0;
-	double m1_percentage_of_m2 = (100.0 - interests_calcul(100.0, rate2, duration2)) / 100.0;
-	double m2_percentage_of_m4 = (100.0 + interests_calcul(100.0, rate2, duration2)) / 100.0;
 	
+	//pourcentage du petit pret avec interets (m1) par rapport au grand pret avec interets (m2)
+	double m1_percentage_of_m2 = (100.0 - interests_calcul(100.0, rate2, duration2)) / 100.0;
+
+	//pourcentage du grand pret avec interets (m2) par rapport au capital du grand pret (m4)
+	double m2_percentage_of_m4 = (100.0 + interests_calcul(100.0, rate2, duration2)) / 100.0;
+
+	//formule donnant le ratio que represente le capital du petit pret dans le montant global du pret (petit + long) pour que
 	return ((1.0 / m1_percentage_of_m3) / ((1.0 / m1_percentage_of_m3) + (1.0 / (m1_percentage_of_m2 * m2_percentage_of_m4))) * 100.0);
 }
