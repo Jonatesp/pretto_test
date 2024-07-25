ddeea35b198dad771625b1b1e8344978e68d1c09
anbourge <anbourge@student.42.fr>
2024-07-26 00:30:20 +0200

    minor fix

--- README.md
+++
@@ -1,1 +0,0 @@
-# pretto_test
--- third_question.cpp
+++ third_question.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include "second_question.cpp"
 
-int best_interests(std::map<int, double> rate_grid, int long_loan_duration) {
-	double	ret = 0.0;
+void best_interests(std::map<int, double> rate_grid, int long_loan_duration) {
+	double	loan_duration = 0.0;
 	double	best_interests = 1000.0;
 
 	for (std::map<int, double>::iterator it = rate_grid.begin(); it != rate_grid.end(); it++) {
@@ -12,11 +12,10 @@ int best_interests(std::map<int, double> rate_grid, int long_loan_duration) {
 			double current_loan_interests = (interests_calcul(100.0, it->second / 100.0, it->first * 12.0) * current_ratio + interests_calcul(100.0, rate_grid.find(long_loan_duration)->second / 100.0, long_loan_duration * 12.0) * (100.0 - current_ratio)) / 100.0;
 			if (current_loan_interests < best_interests) {
 				best_interests = current_loan_interests;
-				ret = it->first;
+				loan_duration = it->first;
 			}
 		}
 	}
-	std::cout << "Best interests for the short loan with a total loan duration of 25 years based on the grid is the " << ret;
+	std::cout << "Best interests for the short loan with a total loan duration of 25 years based on the grid is the " << loan_duration;
 	std::cout << " years long loan with " << best_interests << " \% of total interests." << std::endl;
-	return (ret);
 }
